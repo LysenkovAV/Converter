45998002209a51f4b7c8412b39afb48f1b130bb5
LysenkovAV <107105542+LysenkovAV@users.noreply.github.com>
2022-07-17 23:09:07 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 # настройки
-TOKEN = '5468147191:AAH5qL8u_0FaGeVQxyjWckPhJGK_6J7hOeo' # ключ доступа к боту
+TOKEN = '' # ключ доступа к боту
 
 API_KEY = '3x5jS6NTl0c2qbhN7M1OHbTP2Wd8igLR' # ключ доступа к API для конвертации валют
 
